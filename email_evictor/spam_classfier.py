893617cfa2b346cdb41ad280348ee46c30db1597
Destiny Bonillas <destinybonillas@csu.fullerton.edu>
2024-04-30 19:57:04 -0700

    Issues with initial dataset -- fixed, and fixing up spam classifier

---
+++ email_evictor/spam_classfier.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+#############################
+
+# Spam Classification Program
+# Contributors: Ricardo Granado Macias, Destiny Bonillas, Paul Kennedy
+# Machine Learning Implemented: Sklearn
+
+#############################
+
+# imports
+import time
+import pandas as pd 
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB, GaussianNB
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+
+
+# collect time it takes for program to run
+begin = time.time()
+
+# csv file 1
+# load data
+dataframe = pd.read_csv("/home/dbonillas/SpamClassify/spam_ham_dataset.csv")
+
+# prints data, can comment out later
+print(dataframe.head())
+
+# prints info on our data, can comment out later
+# print(dataframe.describe())
+
+# add plot to visualize spam vs not
+label_counts = dataframe['label'].value_counts()
+plt.figure(figsize=(8, 4))
+colors = ['blue', 'red']
+label_counts.plot(kind='bar', color=colors)
+plt.title('Distribution of Spam vs Not Spam')
+plt.xlabel('Label')
+plt.ylabel('Frequency')
+plt.xticks(rotation=0)
+plt.show()
+
+# split into training and test data
+x = dataframe["text"]
+y = dataframe["label"]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=3)
+
+# extract features
+count_vec = CountVectorizer()
+features = count_vec.fit_transform(x_train)
+
+# build model
+model = svm.SVC()
+
+# model training start time
+train_begin = time.time()
+
+model.fit(features, y_train)
+
+# model training end time
+train_end = time.time()
+print("Model training time: {:.2f} seconds".format(train_end-train_begin))
+
+# test accuracy
+features_test = count_vec.transform(x_test)
+
+# print accuracy
+print("Accuracy of Model: ", model.score(features_test, y_test))
+
+# determine time @ end of program
+end = time.time()
+print("Overall Program Runtime: {:.2f} seconds".format(end-begin))
+
+
+###########End of Program############
